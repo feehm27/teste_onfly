2f264328a269a5475e6b20ab226e8a349e2db197
Fernanda Fontoura <fernanda.lightbase@sebraemg.com.br>
2025-02-07 13:55:18 -0300

    feat: atualização do swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,6 @@ namespace App\Http\Controllers;
  * @OA\Server(
  *    url="/api/v1"
  * ),
- * @OA\Server(url=L5_SWAGGER_CONST_HOST),
  * @OA\Info(
  *      title="Onfly - Pedido de Viagens",
  *      version="1.0",
